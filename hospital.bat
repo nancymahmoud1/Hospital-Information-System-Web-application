5c83242c88de96bcb3015f2c478e8d78085ec1d9
nancymahmoud1 <nancymahmoud2003@gmail.com>
2024-12-26 10:20:14 +0200

    delete files

--- hospital.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-cd /d C:\Users\yassientawfik\Documents\SBME\Semester04\Data_base\Project\Team_13_Hospital_Website
-call venv\Scripts\activate
-start /b python app.py
-timeout /t 5 > nul
-start http://127.0.0.1:5000
